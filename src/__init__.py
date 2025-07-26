a97dfcd2651b5db7d5110a8eefb28db6092ba72e
wjfwing <24264417+jwan2021@users.noreply.github.com>
2025-07-27 09:37:38 +1000

    Seo (#54)
    * add seo

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,4 @@ from .central_banks import fetch_central_bank_policies, fetch_central_bank_rates
 from .economic_data import fetch_economic_data
 from .forex_data import fetch_forex_cfd_data
 from .htmlgen import generate_html
+from .seo import generate_meta_tags, generate_structured_data, save_seo_files
